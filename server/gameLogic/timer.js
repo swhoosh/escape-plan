1061e6c14cce5575cab243533dfbbe26fda13dea
Wish <wish6424@hotmail.com>
2022-10-23 01:34:00 +0700

    refined some UI
    - remove dot
    - tile now get BIGGER when hovered

--- server/gameLogic/timer.js
+++ server/gameLogic/timer.js
@@ -1,19 +1,25 @@
-export const gameTimer = (io,room_id,timer_interval,turn_time = 10, skipturn) => { //default 10 + 1 second turn timer. give +1 second
-		//clear interval of prev turn
-		if(typeof timer_interval !== 'undefined')
-			clearInterval(timer_interval)
-		//start interval
-		io.to(room_id).emit("timer",turn_time)
-		turn_time--;
-		timer_interval = setInterval(() => {
-			if(turn_time >= 0){
-				io.to(room_id).emit("timer",turn_time)
-				turn_time--;
-			} else {
-				clearInterval(timer_interval)
-				//todo skip turn
-				skipturn(room_id)
-			}
-		}, 1000);
-		return timer_interval
-	}
+export const gameTimer = (
+  io,
+  room_id,
+  timer_interval,
+  turn_time = 10,
+  skipturn
+) => {
+  //default 10 + 1 second turn timer. give +1 second
+  //clear interval of prev turn
+  if (typeof timer_interval !== 'undefined') clearInterval(timer_interval)
+  //start interval
+  io.to(room_id).emit('timer', turn_time)
+  turn_time--
+  timer_interval = setInterval(() => {
+    if (turn_time >= 0) {
+      io.to(room_id).emit('timer', turn_time)
+      turn_time--
+    } else {
+      clearInterval(timer_interval)
+      //todo skip turn
+      skipturn(room_id)
+    }
+  }, 1000)
+  return timer_interval
+}
