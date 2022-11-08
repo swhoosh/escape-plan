bf5789ed90042a972ff72b1722d0f5f9b54fc72f
search <search35453@gmail.com>
2022-11-08 20:54:28 +0700

    button styling

--- src/components/TauntButton.tsx
+++ src/components/TauntButton.tsx
@@ -2,11 +2,18 @@ import React, { useContext } from 'react'
 import { GameContext } from '../App'
 
 export default function TauntButton() {
-  const { gameData, setGameData } = useContext(GameContext)
+  const { gameData } = useContext(GameContext)
   const handleOnClick = () => {
     if (gameData.socket !== undefined) {
       gameData.socket.emit('taunt', gameData.roomID)
     }
   }
-  return <button onClick={handleOnClick}>TauntButton</button>
+  return (
+    <button
+      onClick={handleOnClick}
+      className='join-leave-button bg-purple-500 rounded-xl'
+    >
+      <div className='m-auto'>taunt</div>
+    </button>
+  )
 }
