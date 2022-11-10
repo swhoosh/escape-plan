cc2b51ae49908371e10e98417f673efcf734324d
Wish <70953480+wish6424@users.noreply.github.com>
2022-11-10 12:50:52 +0700

    Delete server/node_modules directory

--- server/node_modules/class-transformer/metadata/TypeMetadata.d.ts
+++
@@ -1,8 +0,0 @@
-import { TypeOptions } from "./ExposeExcludeOptions";
-export declare class TypeMetadata {
-    target: Function;
-    propertyName: string;
-    reflectedType: any;
-    typeFunction: (options?: TypeOptions) => Function;
-    constructor(target: Function, propertyName: string, reflectedType: any, typeFunction: (options?: TypeOptions) => Function);
-}
