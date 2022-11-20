57bfeabf8dce2b4cddc19c2b8bf1badfd4218050
Wish <wish6424@hotmail.com>
2022-11-21 00:13:36 +0700

    Update socket.tsx

--- src/service/socket.tsx
+++ src/service/socket.tsx
@@ -1,6 +1,7 @@
 import { io } from 'socket.io-client'
 
-const serverIP = '192.168.1.33'
+// const serverIP = '192.168.1.33'
+const serverIP = 'localhost'
 
 export const socketMain = io(`${serverIP}:6050/`)
 export const socketChat = io(`${serverIP}:6050/chat`)
