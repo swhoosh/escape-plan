8d7819dfa0964350d96df08506a25f93a63db992
ponyyythima <116328590+ponyyythima@users.noreply.github.com>
2022-11-20 21:03:12 +0700

    playerLeaveScreen

--- src/components/BigNotice.tsx
+++ src/components/BigNotice.tsx
@@ -7,9 +7,14 @@ type BigNoticeProps = {
 
 const BigNotice: React.FC<BigNoticeProps> = ({ text }) => {
   return (
-    <div className='fixed translate-y-1/2 translate-x-1/2 bottom-1/2 right-1/2'>
-      <p className='bg-white'>{text}</p>
+    <div className='
+          rounded-full py-3 px-6 
+          bg-white 
+          fixed translate-y-1/2 translate-x-1/2 bottom-1/2 right-1/2 
+          box-border h-18 w-50 p-2 border-2'>
+      <p className='bg-white text-lime-600'>{text}</p>
     </div>
+   
   )
 }
 
